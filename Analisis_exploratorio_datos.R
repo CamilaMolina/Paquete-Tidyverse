8ec86886a752784323153b5af8aec32ec4aba022
Macarena Soledad Valenzuela Beltrán <52059452+ValenzuelaBeltran@users.noreply.github.com>
2020-05-14 19:39:45 -0400

    Add files via upload

---
+++ Analisis_exploratorio_datos.R
@@ -0,0 +1,5 @@
+################### ANÁLISIS EXPLORATORIO DE DATOS ######################
+# David Cox: regresión de cox: análisis de la supervicencia (procesos estocásticos,
+# diseños experimentales)
+#"NO HAY PREGUNTAS RUTINARIAS EN ESTADÍSTICA, SINO PREGUNTAS ACERCA DE LAS RUTINAS ESTADÍSTICA"
+# EN ESTADÍSTICA NO EXISTE UNA RUTINA, SINO LAS RUTINAS ESTADÍSTICAS FUNCIONAN O NO
