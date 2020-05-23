e7b1d6b304505f020fd7037e46e73c617be9c941
Macarena Valenzuela Beltrán <macarenavalenzuelabeltran@gmail.com>
2020-05-23 17:09:48 -0400

    Upload

--- String.R
+++ String.R
@@ -1,4 +1,7 @@
-############## String, factores y las fechas#############
+
+# String, factores y fechas -----------------------------------------------
+
+
 #aprenderemos a usar las regexps para descubrir patrones en strings
 #es un poco difíciles, pero es muy útil saber porque se utiliza en muchos lenguajes.
 
@@ -106,7 +109,9 @@ str_sort(c("apple", "banana", "eggplant"), locale = "how")
 
 str_order(x, locale = "how")
 
-############################ REGEXP#################
+
+# REGEXP ------------------------------------------------------------------
+
 #Expresiones regulares: nos ayuda a escribir un patrón dentro de un string
 
 #str_view()
@@ -148,3 +153,143 @@ str_view(y, "manzana")
 str_view(y, "^manzana$")#encuentra única y exclusivamente la palabra manzana
 
 
+# Busqueda de expresiones regulares ---------------------------------------
+
+#se utiliza sintaxis dentro de la función buscar
+#esto se abre con el siguiente comando ctrl+shift+f
+
+#\b -> localizar la frontera de un palabra
+
+sum()
+summarise()
+
+#\d -> localizar cualquier dígito
+#\s ->  cualquier espacio en blanco (espacio, tabulador, salto de línea)
+#[abc]-> localizar la a, la b o la c indistintamente: los de conjunto
+#[^abc]-> localizar cualquier cosa excepto la a,b, o c
+
+
+# Forma de seleccionar uno o mas patrones ---------------------------------
+
+#abc| d..m # los dos puntos sirven para señalar dos letras cualquiera.
+#la barra vertical significa cualquier cosa antes y después de la norma
+#ejemplo
+str_view(c("grey", "gray"), "gr(e|a)y")
+
+
+# Cuantas veces aparece un determinado elemento dentro de un patrón -------
+
+#Existen tres símbolos para este efecto
+#? -> 0 o 1 
+#+ -> 1 o más veces # al  menos su existencia
+#* -> 0 o más veces # caulquier cosa
+
+#Ejemplo
+x <- "El año 1888 es el mas largo en números romanos: MDCCCLXXXVIII"
+str_view(x, "CC?")
+str_view(x, "CC+")
+str_view(x, "C[LX]+")
+"colou?r"
+"ba(na)+"
+
+
+# Número de veces que busco determinada silava, palabra o letra -----------
+
+#{n}-> exactamente n repeticiones
+#{n,} -> n o más repeticiones
+#{,m} -> como máximo m repeticiones
+#{n,m} -> entre n y m repeticiones
+
+str_view(x, "C{2}")
+str_view(x, "C{2,}")
+str_view(x, "C{2,3}")
+
+#por defecto estos resultados son gridi. Esto significa que van a intentare
+#hacer la búsqueda en el mayor string posible
+
+#También se puede hacer busqueda con el string mas corto posible (lazy)
+#se deb agragar el signo de interrogación
+str_view(x, "C{2,3}?")
+
+str_view(x, "C[LX]+")#sin espacio para que marque la zona
+str_view(x, "C[LX]+?")
+
+
+# Grupo de expresiones y palabras mas complicadas -------------------------
+#Grupos que se pueden referenciar hacia atrás
+
+fruits <- c("banana", "coco", "papaya", "manzana", "pera", "pepino")
+
+#localizar pares repetidos de letra
+
+str_view(fruits, "(..)\\1")#devuelve todo destacando la zona con el patrón
+str_view(fruits, "(..)\\1", match=TRUE)#devuelve solo las palabras que contengan el patrón
+
+#hay un data set que contiene muchas frutas
+fruit
+str_view(fruit, "(..)\\1", match=TRUE)
+
+str_view("abc-abc", "(...)-\\1")
+str_view("abc-abc-", "(...)-\\1")
+str_view("abc-abc-", "(...)(-)\\1\\2")
+str_view("abc-def-", "(...)(-)\\1\\2")
+
+
+# Encontrar y extraer coincidencias de la expresión regular ---------------
+
+#DEterminar un vector de caracteres coincide con un determinado patrón
+str_detect(fruits, "a")
+
+words
+
+#Cuantas de estas palabras comienzan con letra j
+str_detect(words, "^j")
+sum(str_detect(words, "^j"))
+mean(str_detect(words, "^j"))
+mean(str_detect(words, "^[aeiou]"))
+mean(str_detect(words, "[aeiou]$"))
+
+#como encontrar palabras que no contienen ninguna vocal
+!str_detect(words, "[aeiou]")
+
+
+#encontrar solo las palabras que constan de consonantes:
+#sirve también para detectar elementos que coinciden con un cierto patrón
+
+sum(!str_detect(words, "[aeiou]"))
+sum(str_detect(words, "^[^aeiou]+$"))
+
+f1 <- !str_detect(words, "[aeiou]")
+f2 <- str_detect(words, "^[^aeiou]+$")#[^ que no contenga
+identical(f1,f2)
+
+#¿Cuáles son las palabras que no tienen ninguna vocal?
+words[!str_detect(words, "[aeiou]")]
+
+#hacerlo directamente con un subconjunto lógico
+str_subset(words,"[aeiou]$")
+
+#usar combinaciones de sintaxis
+df <- tibble(
+  word = words,
+  i = seq_along(word)
+)
+df %>% view()
+
+df %>% filter(str_detect(words, "x$"))
+
+str_count(fruits, "a")
+
+str_count(words, "[aeiou]")
+mean(str_count(words, "[aeiou]"))
+
+df %>% 
+  mutate(
+    vowels = str_count(word, "[aeiou]"),
+    consonants = str_count(word, "[^aeiou]")
+  )
+#el str_count no tiene overleaping
+
+str_count("abababababa", "aba")
+str_view("abababababa", "aba")
+str_view_all("abababababa", "aba")
